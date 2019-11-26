6b7a9a4ea67c6f29af551ec88a1d8d080c6178cd
Vladyslav Bezkrylyi <whisker1927@gmail.com>
2019-11-26 20:10:58 +0200

    add netlify

--- src/components/App.js
+++ src/components/App.js
@@ -6,6 +6,7 @@ import Reader from './Reader/Reader';
 const App = () => (
   <BrowserRouter>
     <div className={styles.container}>
+      <h1>HELLO!!!</h1>
       <Switch>
         <Route route="/reader" exact component={Reader} />
         <Redirect to="/reader" />
