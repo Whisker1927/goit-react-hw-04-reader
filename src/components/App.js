bf00579eab36cc6ad2c7e1004dbe1e8b2ac56258
Vladyslav Bezkrylyi <whisker1927@gmail.com>
2019-12-09 22:48:52 +0200

    add redirect

--- src/components/App.js
+++ src/components/App.js
@@ -2,13 +2,19 @@ import React from 'react';
 import { BrowserRouter, Route, Switch, Redirect } from 'react-router-dom';
 import styles from '../styles/index.module.css';
 import Reader from './Reader/Reader';
+import routes from '../routes';
+import publications from '../Data/publication.json';
 
 const App = () => (
   <BrowserRouter>
     <div className={styles.container}>
       <Switch>
-        <Route route="/reader" exact component={Reader} />
-        <Redirect to="/reader" />
+        <Route
+          exact
+          path={routes.HOME}
+          render={props => <Reader {...props} items={publications} />}
+        />
+        <Redirect to={routes.HOME} />
       </Switch>
     </div>
   </BrowserRouter>
--- src/components/Reader/Reader.js
+++ src/components/Reader/Reader.js
@@ -1,71 +1,87 @@
 import React, { Component } from 'react';
 import T from 'prop-types';
-import queryString from 'query-string';
 import Controls from '../Controls/Controls';
 import Counter from '../Counter/Counter';
 import Publication from '../Publication/Publication';
 import publications from '../../Data/publication.json';
 import styles from '../../styles/index.module.css';
 
-//
+const getQuery = queryStr =>
+  Number.parseInt(new URLSearchParams(queryStr).get('item'), 10);
 export default class Reader extends Component {
   static propTypes = {
     location: T.shape().isRequired,
     history: T.shape().isRequired,
+    items: T.shape().isRequired,
   };
 
   componentDidMount() {
-    const { location, history } = this.props;
-    if (!location.search) {
+    const { location, history, items } = this.props;
+
+    const publicationIdx = getQuery(location.search);
+
+    if (!publicationIdx || publicationIdx < 1) {
       history.replace({
-        ...location,
-        search: '?item=1',
+        ...location.pathname,
+        search: 'item=1',
+      });
+    }
+
+    if (publicationIdx > items.length) {
+      history.replace({
+        ...location.pathname,
+        search: `item=${items.length}`,
       });
     }
   }
 
   handlePrevList = () => {
     const { location, history } = this.props;
-    const parsed = queryString.parse(location.search).item;
 
-    if (parsed > 1) {
-      history.push({
-        ...location,
-        search: `?item=${Number(parsed) - 1}`,
-      });
-    }
+    const publicationIdx = getQuery(location.search);
+
+    history.push({
+      ...location.pathname,
+      search: `item=${publicationIdx - 1}`,
+    });
   };
 
   handleNextList = () => {
     const { location, history } = this.props;
-    const parsed = queryString.parse(location.search).item;
 
-    if (parsed < publications.length) {
-      history.push({
-        ...location,
-        search: `?item=${Number(parsed) + 1}`,
-      });
-    }
+    const publicationIdx = getQuery(location.search);
+
+    history.push({
+      ...location.pathname,
+      search: `item=${publicationIdx + 1}`,
+    });
   };
 
   render() {
-    const { location } = this.props;
-    const parsed = queryString.parse(location.search).item;
+    const { items, location } = this.props;
+    let publicationIdx = getQuery(location.search);
+    if (!publicationIdx || publicationIdx < 1) {
+      publicationIdx = 1;
+    }
+
+    if (publicationIdx > items.length) {
+      publicationIdx = items.length;
+    }
     return (
       <div className={styles.reader}>
-        {parsed && (
+        {publicationIdx && (
           <>
             <Controls
               nextArticle={this.handleNextList}
               prevArticle={this.handlePrevList}
-              prevDisabled={Number(parsed)}
+              prevDisabled={Number(publicationIdx)}
               nextDisabled={publications.length}
             />
             <Counter
-              currentArticle={Number(parsed)}
+              currentArticle={Number(publicationIdx)}
               allArticles={publications.length}
             />
-            <Publication article={publications[parsed - 1]} />
+            <Publication article={items[publicationIdx - 1]} />
           </>
         )}
       </div>
